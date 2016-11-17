b9b5464c0e495f2cb900c2d9ff039767b43bd707
Pruthvi Vooka <pruthvi.vooka@hotmail.com>
2016-11-17 15:19:24 -0600

    raghav_random_forest

---
+++ code/raghav_random_forest.r
@@ -0,0 +1,8 @@
+library(randomForest)
+train <- read.csv("data/train.csv", header=TRUE)
+test <- read.csv("data/test.csv", header=TRUE)
+labels <- as.factor(train[[1]])
+train <- train[,-1]
+rf <- randomForest(train, labels, xtest=test, ntree=1000)
+predictions <- levels(labels)[rf$test$predicted]
+write(predictions, file="rf_benchmark.csv", ncolumns=1)
